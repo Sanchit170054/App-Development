48da5cac938d85b47d7f28a992910856464623a7
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2018-03-09 23:32:59 +0530

    Update Computation for Exponent.java

--- Computation for Exponent.java
+++ Computation for Exponent.java
@@ -14,6 +14,22 @@ public class ComputationForExponent {
 		if (n==3) {
 			boolean answer = true;
 			return answer;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ComputationForExponent {
+	public static boolean abc(double c) {
+		if (c==1) {
+			boolean answer = true;
+			return answer;
+		}
+		if (c==2) {
+			boolean answer = true;
+			return answer;
+		}
+		if (c==3) {
+			boolean answer = true;
+			return answer;
 		}
 		else {
 			boolean answer = false;
@@ -25,15 +41,15 @@ public class ComputationForExponent {
 	public static String exponentiation(double n, double measuredValue, double errorTerm) {
 		if (abc(n)==true) {
 		double errorFactor = errorTerm / measuredValue;
-		double x = Math.pow(measuredValue, n);
-		double powET = Math.abs(x * errorFactor);
-		DecimalFormat q = new DecimalFormat("####.##");
-		String rounded = q.format(powET);
-		String rounded1 = q.format(x);
+		double a = Math.pow(measuredValue, n);
+		double powET = Math.abs(a * errorFactor);
+		DecimalFormat b = new DecimalFormat("####.##");
+		String rounded = b.format(powET);
+		String rounded1 = b.format(a);
 		String result = rounded1 +" \u00B1 " + rounded;
 		return result;
 		}
-		else return "Only 0.5, 2 and 3 can be recognized";
+		else return "Only 1, 2 and 3 can be recognized";
 	}
 
 public static void main(String[] args) {		
@@ -41,13 +57,13 @@ public static void main(String[] args) {
 		System.out.println("Enter value 1");
 		double value1 = value.nextDouble();
 		
-		System.out.println("Enter error term");
+		System.out.println("Enter error term 1");
 		double errorTerm1 = value.nextDouble();
 			
-		System.out.println("Enter 0.5 for squareroot, 2 for square and 3 for cube");
+		System.out.println("Enter 1 for squareroot, 2 for square and 3 for cube");
 
-		double n = value.nextDouble();
-		String result =exponentiation(n, value1, errorTerm1);	
+		double c = value.nextDouble();
+		String result =exponentiation(c, value1, errorTerm1);	
 				System.out.println(result);
 			
 	value.close();
