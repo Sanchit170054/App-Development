a16ecbac6a873d8476ad6cae70661cf590c7cd3a
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2018-03-22 19:28:12 +0530

    Update NewWorkbook.java

--- NewWorkbook.java
+++ NewWorkbook.java
@@ -1,8 +1,12 @@
 package excelProjectTestbed;
 
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import java.io.FileOutputStream;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,21 +16,21 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 public class NewWorkbook {
 	public static void main(String[] args) throws IOException 
 	{
-		//Locate path and file of input excel.
+		// Step #1 : Locate path and file of input excel.
 		File inputFile=new File("sample-xlsx-file.xlsx");
 		FileInputStream fis=new FileInputStream(inputFile);
 		XSSFWorkbook inputWorkbook=new XSSFWorkbook(fis);
 		int inputSheetCount=inputWorkbook.getNumberOfSheets();
-	
 		
-		//Locate path and file of output excel.
-		File outputFile=new File("WorkBook Copy.xlsx");
+		
+		// Step #2 : Locate path and file of output excel.
+		File outputFile=new File("New work book.xlsx");
 		FileOutputStream fos=new FileOutputStream(outputFile);
 		
-		//Creating workbook for output excel file.
+		// Step #3 : Creating workbook for output excel file.
 		XSSFWorkbook outputWorkbook=new XSSFWorkbook();
 		
-		// Creating sheets with the same name as appearing in input file.
+		// Step #4 : Creating sheets with the same name as appearing in input file.
 		for(int i=0;i<inputSheetCount;i++) 
                 { 
                   XSSFSheet inputSheet=inputWorkbook.getSheetAt(i); 
@@ -37,17 +41,16 @@ public class NewWorkbook {
                  copySheet(inputSheet,outputSheet); 
                 }
 
-               // Write all the sheets in the new Workbook(testData_Copy.xlsx) using FileOutStream Object
+               // Step #9 : Write all the sheets in the new Workbook(testData_Copy.xlsx) using FileOutStream Object
                outputWorkbook.write(fos); 
-               
-              //At the end of the Program close the FileOutputStream object. 
+              // Step #10 : At the end of the Program close the FileOutputStream object. 
                fos.close(); 
           }
 
            public static void copySheet(XSSFSheet inputSheet,XSSFSheet outputSheet) 
            { 
               int rowCount=inputSheet.getLastRowNum(); 
-                            
+              
                 int currentRowIndex=0; if(rowCount>0)
 		{
 			Iterator rowIterator=inputSheet.iterator();
@@ -57,7 +60,7 @@ public class NewWorkbook {
 				Iterator cellIterator=((XSSFRow) rowIterator.next()).cellIterator();
 				while(cellIterator.hasNext())
 				{
-				//Creating new Row, Cell and Input value in the newly created sheet. 
+				// Step #5-8 : Creating new Row, Cell and Input value in the newly created sheet. 
 					String cellData=cellIterator.next().toString();
 					if(currentCellIndex==0)
 						outputSheet.createRow(currentRowIndex).createCell(currentCellIndex).setCellValue(cellData);
@@ -68,6 +71,7 @@ public class NewWorkbook {
 				}
 				currentRowIndex++;
 			}
+			
 		}
 	}
 }
