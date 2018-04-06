15250532c061f76d4a79773ae10fa9f29057bf1b
Sanchit <34150437+Sanchit170054@users.noreply.github.com>
2018-04-06 23:59:20 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,27 +1,40 @@
-package recursivepuzzle;
 
 import java.util.Scanner;
 public class Palindrome{
-    public static boolean isPal(String s)
+    
+	public static boolean isPal(String s)
     {   
-        if(s.length() == 0 || s.length() == 1)
+    
+		if(s.length() == 0 || s.length() == 1)
             return true; 
-        if(s.charAt(0) == s.charAt(s.length()-1))
-        return isPal(s.substring(1, s.length()-1));
-        return false;
+    
+		if(s.charAt(0) == s.charAt(s.length()-1))
+        
+			return isPal(s.substring(1, s.length()-1));
+        
+		return false;
     }
-    public static void main(String[]args)
+    
+	public static void main(String[]args)
     {
-    	   long start = System.nanoTime();
-    	    long stop = System.nanoTime();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter string :");
-        String string = scanner.nextLine();
+    
+		long start = System.nanoTime();
+    	
+		long stop = System.nanoTime();
+        
+		Scanner s = new Scanner(System.in);
+        
+		System.out.println("Enter string:-");
+        String string = s.nextLine();
+       
         if(isPal(string))
-            System.out.println(string + " is a palindrome");
+            System.out.println(string + "\nis a palindrome");
+        
         else
             System.out.println(string + " is not a palindrome");
+        
         long duration = stop - start;
-	    System.out.println("It took " + duration/1000000 + " milliseconds to compute this last value.");
+	    
+        System.out.println("\nIt took " + duration/1000000 + " milliseconds to compute this last value.");
     }
 }
